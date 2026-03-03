1c2ebbade2d16c299a94dd5c0c14457e2a67fbfb
Marcelo <marcelo@mactus.com.br>
2026-03-03 14:56:15 -0300

    ajustes

--- src/Nfse/BeneficioMunicipal/BeneficioMunicipal.php
+++ src/Nfse/BeneficioMunicipal/BeneficioMunicipal.php
@@ -2,7 +2,9 @@
 
 namespace TecnoSpeed\Plugnotas\Nfse\BeneficioMunicipal;
 
+use FerFabricio\Hydrator\Hydrate;
 use TecnoSpeed\Plugnotas\Abstracts\BuilderAbstract;
+use TecnoSpeed\Plugnotas\Error\InvalidTypeError;
 use TecnoSpeed\Plugnotas\Traits\Communication;
 
 class BeneficioMunicipal extends BuilderAbstract
@@ -30,5 +32,17 @@ class BeneficioMunicipal extends BuilderAbstract
     public function getReducao(): ?Reducao
     {
         return $this->reducao;
+    }
+
+    public static function fromArray($data)
+    {
+        if (!is_array($data)) {
+            throw new InvalidTypeError('Deve ser informado um array.');
+        }
+
+        if (array_key_exists('reducao', $data)) {
+            $data['reducao'] = Reducao::fromArray($data['reducao']);
+        }
+        return Hydrate::toObject(self::class, $data);
     }
 }
